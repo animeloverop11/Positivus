aeed6b85b45da6532146b591767e75c4780ee484
Quadri <animeloverop11@gmail.com>
2024-07-28 13:02:12 -0500

    Random

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,8 +32,8 @@ export const Hero = () => {
             <div className="w-1/2 flex flex-col gap-4 2xl:gap-8">
 
 
-                <h1 className="max-md:text-4xl text-6xl 2xl:text-[110px] font-medium w-[531px] 2xl:w-[950px]">Navigating the digital landscape for success</h1>
-                <p className="max-md:text-md text-xl 2xl:text-4xl font-normal max-md:w-[300px] w-[498px] 2xl:w-[900px] h-auto  "> Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
+                <h1 className="max-md:text-4xl text-6xl 2xl:text-[110px] font-medium w-[400px] 2xl:w-[950px]">Navigating the digital landscape for success</h1>
+                <p className="max-md:text-md text-xl 2xl:text-4xl font-normal max-md:w-[400px]  2xl:w-[900px] h-auto  "> Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
 
                 <Link to="" className="max-md:w-[180px] w-[264px] 2xl:w-[370px] max-md:h-[40px] h-[68px] 2xl:h-[90px] bg-[#191A23] text-white 2xl:text-2xl max-md:rounded-lg rounded-2xl font-normal flex items-center justify-center">Book a consultation</Link>
 
